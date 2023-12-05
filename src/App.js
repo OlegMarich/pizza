4982522d3077e1e8fd99a34f40a7dddda48c458f
OlegM <Marich763@gmail.com>
2023-12-05 18:51:34 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+import './scss/app.scss';
 
 function App() {
   return (
-    <div></div>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканськая" price={500}/>
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
